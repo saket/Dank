a1bab74e13206f837f43174203a399d8358216a5
Saket Narayan <saket@saket.me>
2017-05-16 22:56:34 +0530

    Inbox: avoid force-refreshing messages from remote on every fragment recreate

--- app/src/main/java/me/saket/dank/ui/user/messages/InboxActivity.java
+++ app/src/main/java/me/saket/dank/ui/user/messages/InboxActivity.java
@@ -42,182 +42,183 @@ import me.saket.dank.utils.UrlParser;
 import me.saket.dank.widgets.InboxUI.IndependentExpandablePageLayout;
 import timber.log.Timber;
 
-public class InboxActivity extends DankPullCollapsibleActivity implements MessageFolderFragment.Callbacks {
+public class InboxActivity extends DankPullCollapsibleActivity implements InboxFolderFragment.Callbacks {
 
-    @BindView(R.id.inbox_root) IndependentExpandablePageLayout contentPage;
-    @BindView(R.id.inbox_tablayout) TabLayout tabLayout;
-    @BindView(R.id.inbox_viewpager) ViewPager viewPager;
+  @BindView(R.id.inbox_root) IndependentExpandablePageLayout contentPage;
+  @BindView(R.id.inbox_tablayout) TabLayout tabLayout;
+  @BindView(R.id.inbox_viewpager) ViewPager viewPager;
 
-    private BehaviorRelay<List<Message>> unreadMessageStream = BehaviorRelay.create();
-    private BehaviorRelay<List<Message>> privateMessageStream = BehaviorRelay.create();
-    private BehaviorRelay<List<Message>> commentRepliesStream = BehaviorRelay.create();
-    private BehaviorRelay<List<Message>> postRepliesStream = BehaviorRelay.create();
-    private BehaviorRelay<List<Message>> usernameMentionStream = BehaviorRelay.create();
+  private BehaviorRelay<List<Message>> unreadMessageStream = BehaviorRelay.create();
+  private BehaviorRelay<List<Message>> privateMessageStream = BehaviorRelay.create();
+  private BehaviorRelay<List<Message>> commentRepliesStream = BehaviorRelay.create();
+  private BehaviorRelay<List<Message>> postRepliesStream = BehaviorRelay.create();
+  private BehaviorRelay<List<Message>> usernameMentionStream = BehaviorRelay.create();
 
-    private DankLinkMovementMethod commentLinkMovementMethod;
+  private DankLinkMovementMethod commentLinkMovementMethod;
 
-    /**
-     * @param expandFromShape The initial shape from where this Activity will begin its entry expand animation.
-     */
-    public static void start(Context context, @Nullable Rect expandFromShape) {
-        context.startActivity(createStartIntent(context, expandFromShape));
+  /**
+   * @param expandFromShape The initial shape from where this Activity will begin its entry expand animation.
+   */
+  public static void start(Context context, @Nullable Rect expandFromShape) {
+    context.startActivity(createStartIntent(context, expandFromShape));
+  }
+
+  public static Intent createStartIntent(Context context, @Nullable Rect expandFromShape) {
+    Intent intent = new Intent(context, InboxActivity.class);
+    intent.putExtra(KEY_EXPAND_FROM_SHAPE, expandFromShape);
+    return intent;
+  }
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_messages);
+    ButterKnife.bind(this);
+    findAndSetupToolbar();
+
+    setupContentExpandablePage(contentPage);
+    expandFrom(getIntent().getParcelableExtra(KEY_EXPAND_FROM_SHAPE));
+
+    MessagesPagerAdapter messagesPagerAdapter = new MessagesPagerAdapter(getResources(), getSupportFragmentManager());
+    viewPager.setAdapter(messagesPagerAdapter);
+    tabLayout.setupWithViewPager(viewPager, true);
+
+    contentPage.setPullToCollapseIntercepter((event, downX, downY, upwardPagePull) -> {
+      //noinspection SimplifiableIfStatement
+      if (touchLiesOn(viewPager, downX, downY)) {
+        InboxFolderFragment activeFragment = messagesPagerAdapter.getFragment(viewPager.getCurrentItem());
+        return activeFragment.shouldInterceptPullToCollapse(upwardPagePull);
+      } else {
+        return false;
+      }
+    });
+
+    unsubscribeOnDestroy(RxViewPager.pageSelections(viewPager)
+        .scan((prevPage, newPage) -> {
+          InboxFolderFragment previousFragment = messagesPagerAdapter.getFragment(prevPage);
+          if (previousFragment != null) {
+            previousFragment.onFragmentHiddenInPager();
+          }
+          return newPage;
+        })
+        .subscribe()
+    );
+  }
+
+  /**
+   * These local streams are used as a cache of messages loaded so far, so that we can
+   * combine all message pages loaded so far and emit them to the fragment subscribers.
+   */
+  @Override
+  public BehaviorRelay<List<Message>> messageStream(InboxFolder folder) {
+    switch (folder) {
+      case UNREAD:
+        return unreadMessageStream;
+
+      case PRIVATE_MESSAGES:
+        return privateMessageStream;
+
+      case COMMENT_REPLIES:
+        return commentRepliesStream;
+
+      case POST_REPLIES:
+        return postRepliesStream;
+
+      case USERNAME_MENTIONS:
+        return usernameMentionStream;
+
+      default:
+        throw new UnsupportedOperationException("Unknown message folder: " + folder);
     }
+  }
 
-    public static Intent createStartIntent(Context context, @Nullable Rect expandFromShape) {
-        Intent intent = new Intent(context, InboxActivity.class);
-        intent.putExtra(KEY_EXPAND_FROM_SHAPE, expandFromShape);
-        return intent;
+  @Override
+  public InfiniteScroller.InfiniteDataStreamFunction<Message> fetchMoreMessages(InboxFolder folder) {
+    return (paginationAnchor, skipCache) -> {
+      BehaviorRelay<List<Message>> folderStream = messageStream(folder);
+      MessageCacheKey cacheKey = MessageCacheKey.create(folder, paginationAnchor);
+
+      Single<List<Message>> storeStream;
+      if (skipCache) {
+        storeStream = Dank.stores().messageStore().fetch(cacheKey);
+        //.map(m -> m.subList(m.size() - Math.min(m.size(), 9), m.size() - Math.min(m.size(), 1)))
+      } else {
+        storeStream = Dank.stores().messageStore().get(cacheKey)
+            // Using a set here to avoid duplicates. Duplicates can creep in when force-loading messages.
+            .map(nextPageMessages -> ImmutableList.copyOf(new ImmutableSet.Builder<Message>()
+                .addAll(folderStream.hasValue() ? folderStream.getValue() : Collections.emptyList())
+                .addAll(nextPageMessages)
+                .build()));
+      }
+
+      return storeStream.doOnSuccess(messages -> folderStream.accept(messages));
+    };
+  }
+
+  @Override
+  public BetterLinkMovementMethod getMessageLinkMovementMethod() {
+    if (commentLinkMovementMethod == null) {
+      commentLinkMovementMethod = DankLinkMovementMethod.newInstance();
+      commentLinkMovementMethod.setOnLinkClickListener((textView, url) -> {
+        // TODO: 18/03/17 Remove try/catch block
+        try {
+          Link parsedLink = UrlParser.parse(url);
+          Point clickedUrlCoordinates = commentLinkMovementMethod.getLastUrlClickCoordinates();
+          int deviceDisplayWidth = getResources().getDisplayMetrics().widthPixels;
+          Rect clickedUrlCoordinatesRect = new Rect(0, clickedUrlCoordinates.y, deviceDisplayWidth, clickedUrlCoordinates.y);
+          OpenUrlActivity.handle(this, parsedLink, clickedUrlCoordinatesRect);
+          return true;
+
+        } catch (Exception e) {
+          Timber.i(e, "Couldn't parse URL: %s", url);
+          return false;
+        }
+      });
+    }
+    return commentLinkMovementMethod;
+  }
+
+  public static class MessagesPagerAdapter extends FragmentStatePagerAdapter {
+    private Resources resources;
+    private SparseArray<InboxFolderFragment> fragmentMap;
+
+    public MessagesPagerAdapter(Resources resources, FragmentManager manager) {
+      super(manager);
+      this.resources = resources;
+      this.fragmentMap = new SparseArray<>();
     }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_messages);
-        ButterKnife.bind(this);
-        findAndSetupToolbar();
-
-        setupContentExpandablePage(contentPage);
-        expandFrom(getIntent().getParcelableExtra(KEY_EXPAND_FROM_SHAPE));
-
-        MessagesPagerAdapter messagesPagerAdapter = new MessagesPagerAdapter(getResources(), getSupportFragmentManager());
-        viewPager.setAdapter(messagesPagerAdapter);
-        tabLayout.setupWithViewPager(viewPager, true);
-
-        contentPage.setPullToCollapseIntercepter((event, downX, downY, upwardPagePull) -> {
-            //noinspection SimplifiableIfStatement
-            if (touchLiesOn(viewPager, downX, downY)) {
-                MessageFolderFragment activeFragment = messagesPagerAdapter.getFragment(viewPager.getCurrentItem());
-                return activeFragment.shouldInterceptPullToCollapse(upwardPagePull);
-            } else {
-                return false;
-            }
-        });
-
-        unsubscribeOnDestroy(RxViewPager.pageSelections(viewPager)
-                .scan((prevPage, newPage) -> {
-                    MessageFolderFragment previousFragment = messagesPagerAdapter.getFragment(prevPage);
-                    if (previousFragment != null) {
-                        previousFragment.onFragmentHiddenInPager();
-                    }
-                    return newPage;
-                })
-                .subscribe()
-        );
+    public Fragment getItem(int position) {
+      return InboxFolderFragment.create(InboxFolder.ALL[position]);
     }
 
     @Override
-    public InfiniteScroller.InfiniteDataStreamFunction<Message> fetchMoreMessages(InboxFolder folder) {
-        return (paginationAnchor, skipCache) -> {
-            BehaviorRelay<List<Message>> folderStream = messageStream(folder);
-            MessageCacheKey cacheKey = MessageCacheKey.create(folder, paginationAnchor);
-
-            Single<List<Message>> storeStream;
-            if (skipCache) {
-                storeStream = Dank.stores().messageStore().fetch(cacheKey);
-                //.map(m -> m.subList(m.size() - Math.min(m.size(), 9), m.size() - Math.min(m.size(), 1)))
-            } else {
-                storeStream = Dank.stores().messageStore().get(cacheKey)
-                        // Using a set here to avoid duplicates. Duplicates can creep in when force-loading messages.
-                        .map(nextPageMessages -> ImmutableList.copyOf(new ImmutableSet.Builder<Message>()
-                                .addAll(folderStream.hasValue() ? folderStream.getValue() : Collections.emptyList())
-                                .addAll(nextPageMessages)
-                                .build()));
-            }
-
-            return storeStream.doOnSuccess(messages -> folderStream.accept(messages));
-        };
-    }
-
-    /**
-     * These local streams are used as a cache of messages loaded so far, so that we can
-     * combine all message pages loaded so far and emit them to the fragment subscribers.
-     */
-    private BehaviorRelay<List<Message>> messageStream(InboxFolder folder) {
-        switch (folder) {
-            case UNREAD:
-                return unreadMessageStream;
-
-            case PRIVATE_MESSAGES:
-                return privateMessageStream;
-
-            case COMMENT_REPLIES:
-                return commentRepliesStream;
-
-            case POST_REPLIES:
-                return postRepliesStream;
-
-            case USERNAME_MENTIONS:
-                return usernameMentionStream;
-
-            default:
-                throw new UnsupportedOperationException("Unknown message folder: " + folder);
-        }
+    public int getCount() {
+      return InboxFolder.ALL.length;
     }
 
     @Override
-    public BetterLinkMovementMethod getMessageLinkMovementMethod() {
-        if (commentLinkMovementMethod == null) {
-            commentLinkMovementMethod = DankLinkMovementMethod.newInstance();
-            commentLinkMovementMethod.setOnLinkClickListener((textView, url) -> {
-                // TODO: 18/03/17 Remove try/catch block
-                try {
-                    Link parsedLink = UrlParser.parse(url);
-                    Point clickedUrlCoordinates = commentLinkMovementMethod.getLastUrlClickCoordinates();
-                    int deviceDisplayWidth = getResources().getDisplayMetrics().widthPixels;
-                    Rect clickedUrlCoordinatesRect = new Rect(0, clickedUrlCoordinates.y, deviceDisplayWidth, clickedUrlCoordinates.y);
-                    OpenUrlActivity.handle(this, parsedLink, clickedUrlCoordinatesRect);
-                    return true;
-
-                } catch (Exception e) {
-                    Timber.i(e, "Couldn't parse URL: %s", url);
-                    return false;
-                }
-            });
-        }
-        return commentLinkMovementMethod;
+    public CharSequence getPageTitle(int position) {
+      return resources.getString(InboxFolder.ALL[position].titleRes());
     }
 
-    public static class MessagesPagerAdapter extends FragmentStatePagerAdapter {
-        private Resources resources;
-        private SparseArray<MessageFolderFragment> fragmentMap;
-
-        public MessagesPagerAdapter(Resources resources, FragmentManager manager) {
-            super(manager);
-            this.resources = resources;
-            this.fragmentMap = new SparseArray<>();
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return MessageFolderFragment.create(InboxFolder.ALL[position]);
-        }
-
-        @Override
-        public int getCount() {
-            return InboxFolder.ALL.length;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return resources.getString(InboxFolder.ALL[position].titleRes());
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            MessageFolderFragment fragment = (MessageFolderFragment) super.instantiateItem(container, position);
-            fragmentMap.put(position, fragment);
-            return fragment;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            fragmentMap.remove(position);
-            super.destroyItem(container, position, object);
-        }
-
-        public MessageFolderFragment getFragment(int position) {
-            return fragmentMap.get(position);
-        }
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+      InboxFolderFragment fragment = (InboxFolderFragment) super.instantiateItem(container, position);
+      fragmentMap.put(position, fragment);
+      return fragment;
     }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+      fragmentMap.remove(position);
+      super.destroyItem(container, position, object);
+    }
+
+    public InboxFolderFragment getFragment(int position) {
+      return fragmentMap.get(position);
+    }
+  }
 
 }
---
+++ app/src/main/java/me/saket/dank/ui/user/messages/InboxFolderFragment.java
@@ -0,0 +1,251 @@
+package me.saket.dank.ui.user.messages;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.jakewharton.rxrelay2.BehaviorRelay;
+
+import net.dean.jraw.models.Listing;
+import net.dean.jraw.models.Message;
+import net.dean.jraw.models.PrivateMessage;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import me.saket.bettermovementmethod.BetterLinkMovementMethod;
+import me.saket.dank.R;
+import me.saket.dank.data.PaginationAnchor;
+import me.saket.dank.data.ResolvedError;
+import me.saket.dank.di.Dank;
+import me.saket.dank.ui.DankFragment;
+import me.saket.dank.utils.InfiniteScrollListener;
+import me.saket.dank.utils.InfiniteScrollRecyclerAdapter;
+import me.saket.dank.utils.InfiniteScrollRecyclerAdapter.ProgressType;
+import me.saket.dank.utils.InfiniteScroller;
+import me.saket.dank.widgets.EmptyStateView;
+import me.saket.dank.widgets.ErrorStateView;
+import timber.log.Timber;
+
+/**
+ * Displays messages under one folder. e.g., "Unread", "Messages", "Comment replies", etc.
+ */
+public class InboxFolderFragment extends DankFragment {
+
+  private static final String KEY_FOLDER = "folder";
+
+  @BindView(R.id.messagefolder_message_list) RecyclerView messageList;
+  @BindView(R.id.messagefolder_first_load_progress) View firstLoadProgressView;
+  @BindView(R.id.messagefolder_empty_state) EmptyStateView emptyStateView;
+  @BindView(R.id.messagefolder_error_state) ErrorStateView firstLoadErrorStateView;
+
+  private InboxFolder folder;
+  private MessagesAdapter messagesAdapter;
+  private InfiniteScrollRecyclerAdapter messagesAdapterWithProgress;
+  private Snackbar loadMoreErrorSnackbar;     // TODO: Rxify this.
+
+  interface Callbacks {
+    BehaviorRelay<List<Message>> messageStream(InboxFolder folder);
+
+    InfiniteScroller.InfiniteDataStreamFunction<Message> fetchMoreMessages(InboxFolder folder);
+
+    BetterLinkMovementMethod getMessageLinkMovementMethod();
+  }
+
+  public static InboxFolderFragment create(InboxFolder folder) {
+    InboxFolderFragment fragment = new InboxFolderFragment();
+    Bundle arguments = new Bundle(1);
+    arguments.putSerializable(KEY_FOLDER, folder);
+    fragment.setArguments(arguments);
+    return fragment;
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    super.onCreateView(inflater, container, savedInstanceState);
+    View layout = inflater.inflate(R.layout.fragment_message_folder, container, false);
+    ButterKnife.bind(this, layout);
+    return layout;
+  }
+
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    folder = (InboxFolder) getArguments().getSerializable(KEY_FOLDER);
+
+    messagesAdapter = new MessagesAdapter(((Callbacks) getActivity()).getMessageLinkMovementMethod());
+    messagesAdapterWithProgress = InfiniteScrollRecyclerAdapter.wrap(messagesAdapter);
+    messageList.setAdapter(messagesAdapterWithProgress);
+    messageList.setLayoutManager(new LinearLayoutManager(getActivity()));
+    messageList.setItemAnimator(new DefaultItemAnimator());
+
+    unsubscribeOnDestroy(((Callbacks) getActivity()).messageStream(folder)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(messagesAdapter));
+
+    startInfiniteScroll();
+
+    populateEmptyStateView();
+    firstLoadErrorStateView.setOnRetryClickListener(o -> startInfiniteScroll());
+  }
+
+  /**
+   * Called when the ViewPager is swiped and this page goes out of the view.
+   */
+  public void onFragmentHiddenInPager() {
+    if (loadMoreErrorSnackbar != null) {
+      loadMoreErrorSnackbar.dismiss();
+    }
+  }
+
+  private void startInfiniteScroll() {
+    InfiniteScroller.ViewStateCallbacks<Message> viewStateCallbacks = new InfiniteScroller.ViewStateCallbacks<Message>() {
+      @Override
+      public void setFirstLoadProgressVisible(boolean visible) {
+        firstLoadProgressView.setVisibility(visible ? View.VISIBLE : View.GONE);
+      }
+
+      @Override
+      public void setMoreLoadProgressVisible(boolean visible) {
+        messageList.post(() -> messagesAdapterWithProgress.setProgressVisible(ProgressType.MORE_DATA_LOAD_PROGRESS, visible));
+      }
+
+      @Override
+      public void setRefreshProgressVisible(boolean visible) {
+        //Timber.i("setRefreshProgressVisible() -> visible: %s", visible);
+        messageList.post(() -> messagesAdapterWithProgress.setProgressVisible(ProgressType.REFRESH_DATA_LOAD_PROGRESS, visible));
+      }
+
+      @Override
+      public void setEmptyStateVisible(boolean visible) {
+        emptyStateView.setVisibility(visible ? View.VISIBLE : View.GONE);
+      }
+
+      @Override
+      public void updateItemsDataset(List<Message> items) {
+        // We avoid updating the data-set here. Instead, InboxActivity stores all fetched items
+        // locally and we subscribe to its stream in onViewCreated(). This way, we can avoid
+        // force-refreshing messages everytime this fragment gets recreated.
+      }
+
+      @Override
+      public void setErrorOnFirstLoadVisible(@Nullable Throwable error, boolean errorVisible) {
+        firstLoadErrorStateView.setVisibility(errorVisible ? View.VISIBLE : View.GONE);
+
+        if (errorVisible) {
+          ResolvedError resolvedError = Dank.errors().resolve(error);
+          if (resolvedError.isUnknown()) {
+            Timber.e(error, "Couldn't fetch more messages under %s", folder);
+          }
+          firstLoadErrorStateView.applyFrom(resolvedError);
+        }
+      }
+
+      @Override
+      public void setErrorOnMoreLoadVisible(@Nullable Throwable error, boolean errorVisible) {
+        if (!isVisible()) {
+          // Ignore showing the snackbar if this fragment isn't visible,
+          // because the Snackbar is appearing in the Activity's layout.
+          return;
+        }
+
+        if (errorVisible) {
+          ResolvedError resolvedError = Dank.errors().resolve(error);
+          if (resolvedError.isUnknown()) {
+            Timber.e(error, "Couldn't fetch more messages under %s", folder);
+          }
+
+          loadMoreErrorSnackbar = Snackbar.make(messageList, resolvedError.errorMessageRes(), Snackbar.LENGTH_INDEFINITE);
+          loadMoreErrorSnackbar.setAction(R.string.common_error_retry, v -> startInfiniteScroll());
+          loadMoreErrorSnackbar.show();
+
+        } else if (loadMoreErrorSnackbar != null) {
+          loadMoreErrorSnackbar.dismiss();
+        }
+      }
+    };
+
+    InfiniteScrollListener scrollListener = InfiniteScrollListener.create(messageList, 0.75f /* loadThreshold */);
+    boolean firstLoadDone = ((Callbacks) getActivity()).messageStream(folder).hasValue();
+    scrollListener.setEmitInitialEvent(!firstLoadDone);
+
+    unsubscribeOnDestroy(
+        InfiniteScroller.<Message>create(scrollListener)
+            .withViewStateCallbacks(viewStateCallbacks)
+            .withStreams(paginationAnchorStream(), ((Callbacks) getActivity()).fetchMoreMessages(folder))
+            .subscribe()
+    );
+  }
+
+  public boolean shouldInterceptPullToCollapse(boolean upwardPagePull) {
+    return messageList.canScrollVertically(upwardPagePull ? +1 : -1);
+  }
+
+  /**
+   * Find the full-name of the last message in the list, that can be used as the anchor for
+   * fetching the next set of pages.
+   */
+  private Single<PaginationAnchor> paginationAnchorStream() {
+    return Single.fromCallable(() -> {
+      if (messagesAdapter.getItemCount() > 0) {
+        List<Message> existingMessages = messagesAdapter.getData();
+        Message lastMessage = existingMessages.get(existingMessages.size() - 1);
+
+        // Private messages can have nested replies. Go through them and find the last one.
+        if (lastMessage instanceof PrivateMessage) {
+          JsonNode repliesNode = lastMessage.getDataNode().get("replies");
+          if (repliesNode.isObject()) {
+            // Replies are present.
+            //noinspection MismatchedQueryAndUpdateOfCollection
+            Listing<Message> lastMessageReplies = new Listing<>(repliesNode.get("data"), Message.class);
+            Message lastMessageLastReply = lastMessageReplies.get(lastMessageReplies.size() - 1);
+            return PaginationAnchor.create(lastMessageLastReply.getFullName());
+          }
+        }
+
+        return PaginationAnchor.create(lastMessage.getFullName());
+
+      } else {
+        return PaginationAnchor.createEmpty();
+      }
+    });
+  }
+
+  private void populateEmptyStateView() {
+    emptyStateView.setEmoji(R.string.inbox_empty_state_title);
+
+    switch (folder) {
+      case UNREAD:
+        emptyStateView.setMessage(R.string.inbox_empty_state_message_for_unread);
+        break;
+
+      case PRIVATE_MESSAGES:
+        emptyStateView.setMessage(R.string.inbox_empty_state_message_for_private_messages);
+        break;
+
+      case COMMENT_REPLIES:
+        emptyStateView.setMessage(R.string.inbox_empty_state_message_for_comment_replies);
+        break;
+
+      case POST_REPLIES:
+        emptyStateView.setMessage(R.string.inbox_empty_state_message_for_post_replies);
+        break;
+
+      case USERNAME_MENTIONS:
+        emptyStateView.setMessage(R.string.inbox_empty_state_message_for_username_mentions);
+        break;
+    }
+  }
+
+}
