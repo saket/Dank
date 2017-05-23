f9f050950975757ff681a2ff79df0da9301d054c
Saket Narayan <saket@saket.me>
2017-05-23 05:52:22 +0530

    Remove unused files
    RedditPaginator
    PaginationCompleteException
    Images
    Strings
    SimpleOnChildAttachStateChangeListener

--- app/src/main/java/me/saket/dank/utils/Strings.java
+++ app/src/main/java/me/saket/dank/utils/Strings.java
@@ -4,7 +4,6 @@ import android.content.res.Resources;
 
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 
 import me.saket.dank.R;
 
@@ -12,29 +11,6 @@ import me.saket.dank.R;
  * Utility methods for String manipulation.
  */
 public class Strings {
-
-  /**
-   * "A, B, C and D".
-   *
-   * TODO: Remove
-   */
-  public static String concatenateWithCommaAndAnd2(Resources resources, List<String> strings) {
-    final StringBuilder stringBuilder = new StringBuilder(strings.size());
-    String comma = resources.getString(R.string.comma_separator_for_multiple_words);
-    String and = resources.getString(R.string.and_suffix_for_comma_separated_words);
-
-    stringBuilder.append(strings.get(0));
-    for (int index = 1; index < strings.size(); index++) {
-      if (index == strings.size() - 1) {
-        stringBuilder.append(" ").append(and).append(" ");
-      } else {
-        stringBuilder.append(comma).append(" ");
-      }
-      stringBuilder.append(strings.get(index));
-    }
-
-    return stringBuilder.toString();
-  }
 
   public static String concatenateWithCommaAndAnd(Resources resources, Collection<String> strings) {
     final StringBuilder stringBuilder = new StringBuilder(strings.size());
@@ -59,5 +35,4 @@ public class Strings {
 
     return stringBuilder.toString();
   }
-
 }
