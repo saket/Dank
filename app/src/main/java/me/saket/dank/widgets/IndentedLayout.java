18245c33127f472b2f834e22b3d6ec066e583476
Saket Narayan <saket@saket.me>
2017-06-20 08:04:51 +0530

    [FIX] IndentedLayout: draw dashed lines using Path to avoid using software layer

--- app/src/main/java/me/saket/dank/widgets/IndentedLayout.java
+++ app/src/main/java/me/saket/dank/widgets/IndentedLayout.java
@@ -6,12 +6,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.IntRange;
 import android.support.annotation.Nullable;
 import android.support.annotation.Px;
 import android.util.AttributeSet;
 import android.util.TypedValue;
-import android.view.View;
 import android.widget.LinearLayout;
 
 import me.saket.dank.R;
@@ -24,6 +24,7 @@ public class IndentedLayout extends LinearLayout {
   private int indentationDepth;
   private final int spacePerDepthPx;
   private Paint indentationLinePaint;
+  private Path indentationLinePath;
 
   private int originalPaddingStart;
 
@@ -40,22 +41,31 @@ public class IndentedLayout extends LinearLayout {
     int indentationLineWidth = attributes.getDimensionPixelSize(R.styleable.IndentedLayout_indentationLineWidth, dpToPx(DEFAULT_LINE_WIDTH, context));
     attributes.recycle();
 
+    // Using a Path so that dashes can be rendered
+    indentationLinePath = new Path();
     indentationLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     indentationLinePaint.setColor(indentationLineColor);
     indentationLinePaint.setStrokeWidth(indentationLineWidth);
     indentationLinePaint.setStyle(Paint.Style.FILL_AND_STROKE);
     indentationLinePaint.setPathEffect(new DashPathEffect(new float[] { indentationLineWidth * 2, indentationLineWidth * 2 }, 0));
-    setLayerType(View.LAYER_TYPE_SOFTWARE, null);   // Dashes aren't supported on hardware layer.
+  }
+
+  @Override
+  protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    super.onLayout(changed, l, t, r, b);
+
+    indentationLinePath.reset();
+    for (int i = 0; i < indentationDepth; i++) {
+      float lineStartX = spacePerDepthPx * (i + 1) + indentationLinePaint.getStrokeWidth();
+      indentationLinePath.moveTo(lineStartX, 0);
+      indentationLinePath.lineTo(lineStartX, getHeight());
+    }
   }
 
   @Override
   public void draw(Canvas canvas) {
     super.draw(canvas);
-
-    for (int i = 0; i < indentationDepth; i++) {
-      float lineStartX = spacePerDepthPx * (i + 1) + indentationLinePaint.getStrokeWidth();
-      canvas.drawLine(lineStartX, 0, lineStartX, getHeight(), indentationLinePaint);
-    }
+    canvas.drawPath(indentationLinePath, indentationLinePaint);
   }
 
   public void setIndentationDepth(@IntRange(from = 0, to = 1) int depth) {
