db939656a851570d863fd7e48b4b2ada043c0938
Saket Narayan <saket@saket.me>
2017-07-29 21:21:10 +0530

    [WIP] MediaAlbumViewer: fade-in photos

--- app/src/main/java/me/saket/dank/ui/media/MediaFragment.java
+++ app/src/main/java/me/saket/dank/ui/media/MediaFragment.java
@@ -1,6 +1,7 @@
 package me.saket.dank.ui.media;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -14,8 +15,11 @@ import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import me.saket.dank.R;
 import me.saket.dank.utils.GlidePaddingTransformation;
+import me.saket.dank.utils.GlideUtils;
 import me.saket.dank.widgets.ZoomableImageView;
 import me.saket.dank.widgets.binoculars.FlickDismissLayout;
 import me.saket.dank.widgets.binoculars.FlickGestureListener;
@@ -27,7 +31,8 @@ public class MediaFragment extends Fragment {
 
   private static final String KEY_MEDIA_ITEM = "mediaItem";
 
-  private ZoomableImageView imageView;
+  @BindView(R.id.imageviewer_imageview) ZoomableImageView imageView;
+  @BindView(R.id.imageviewer_flickdismisslayout) FlickDismissLayout imageContainerView;
 
   interface OnMediaItemClickListener {
     void onClickMediaItem();
@@ -56,18 +61,16 @@ public class MediaFragment extends Fragment {
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_page_photo, container, false);
+    View layout = inflater.inflate(R.layout.fragment_page_photo, container, false);
+    ButterKnife.bind(this, layout);
+    return layout;
   }
 
   @Override
   public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    imageView = (ZoomableImageView) view.findViewById(R.id.photopage_imageview);
-    FlickDismissLayout imageContainerView = (FlickDismissLayout) view.findViewById(R.id.photopage_flickdismisslayout);
 
-    if (view.getParent() instanceof ViewPager) {
-      imageView.enableScrollInViewPager((ViewPager) view.getParent());
-    }
+    imageView.enableScrollInViewPager((ViewPager) view.getParent());
     imageView.setGestureRotationEnabled(true);
 
     //noinspection ConstantConditions
@@ -75,18 +78,27 @@ public class MediaFragment extends Fragment {
     int deviceDisplayWidth = getResources().getDisplayMetrics().widthPixels;
     assert mediaAlbumItem != null;
 
-    GlidePaddingTransformation glidePaddingTransformation = new GlidePaddingTransformation(getActivity(), Color.TRANSPARENT) {
-      final Size onePxTransparentBorder = new Size(1, 1);
-      @Override
-      public Size getPadding(int imageWidth, int imageHeight) {
-        return onePxTransparentBorder;
-      }
-    };
-
+    imageView.setVisibility(View.INVISIBLE);
     Glide.with(this)
         .load(mediaAlbumItem.mediaLink().optimizedImageUrl(deviceDisplayWidth))
         .asBitmap()
-        .transform(glidePaddingTransformation)
+        .transform(new GlidePaddingTransformation(getActivity(), Color.TRANSPARENT) {
+          @Override
+          public Size getPadding(int imageWidth, int imageHeight) {
+            return new Size(0, 0);
+          }
+        })
+        .listener(new GlideUtils.SimpleRequestListener<String, Bitmap>() {
+          @Override
+          public void onResourceReady(Bitmap resource) {
+            imageView.setVisibility(View.VISIBLE);
+          }
+
+          @Override
+          public void onException(Exception e) {
+            super.onException(e);
+          }
+        })
         .into(imageView);
 
     // Make the image flick-dismissible.
