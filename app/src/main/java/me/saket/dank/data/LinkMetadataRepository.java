1d4b756ef03db1b2021d79c302f2ec92194db182
Saket Narayan <saket@saket.me>
2018-03-15 22:47:42 +0530

    Add MoshiStoreJsonParser

--- app/src/main/java/me/saket/dank/data/LinkMetadataRepository.java
+++ app/src/main/java/me/saket/dank/data/LinkMetadataRepository.java
@@ -6,10 +6,8 @@ import com.nytimes.android.external.fs3.PathResolver;
 import com.nytimes.android.external.fs3.filesystem.FileSystem;
 import com.nytimes.android.external.store3.base.impl.Store;
 import com.nytimes.android.external.store3.base.impl.StoreBuilder;
-import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 
-import java.io.IOException;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -19,9 +17,9 @@ import me.saket.dank.BuildConfig;
 import me.saket.dank.data.links.Link;
 import me.saket.dank.data.links.LinkMetadata;
 import me.saket.dank.di.DankApi;
+import me.saket.dank.utils.MoshiStoreJsonParser;
 import me.saket.dank.utils.StoreFilePersister;
 import me.saket.dank.utils.Urls;
-import okio.BufferedSource;
 
 @Singleton
 public class LinkMetadataRepository {
@@ -34,7 +32,7 @@ public class LinkMetadataRepository {
       String url = key.unparsedUrl();
       return Urls.parseDomainName(url) + "_" + Urls.parseFileNameWithExtension(url) + "_" + url.hashCode();
     };
-    StoreFilePersister.JsonParser<Link, LinkMetadata> jsonParser = new LinkMetadataStoreJsonParser(moshi);
+    StoreFilePersister.JsonParser<Link, LinkMetadata> jsonParser = new MoshiStoreJsonParser<>(moshi, LinkMetadata.class);
 
     linkMetadataStore = StoreBuilder.<Link, LinkMetadata>key()
         .fetcher(link -> unfurlLinkFromRemote(dankApi, link))
@@ -53,24 +51,6 @@ public class LinkMetadataRepository {
       throw new IllegalStateException();
     }
     return Completable.fromAction(() -> linkMetadataStore.clear());
-  }
-
-  private static class LinkMetadataStoreJsonParser implements StoreFilePersister.JsonParser<Link, LinkMetadata> {
-    private final JsonAdapter<LinkMetadata> jsonAdapter;
-
-    public LinkMetadataStoreJsonParser(Moshi moshi) {
-      this.jsonAdapter = moshi.adapter(LinkMetadata.class);
-    }
-
-    @Override
-    public LinkMetadata fromJson(BufferedSource jsonBufferedSource) throws IOException {
-      return jsonAdapter.fromJson(jsonBufferedSource);
-    }
-
-    @Override
-    public String toJson(LinkMetadata value) {
-      return jsonAdapter.toJson(value);
-    }
   }
 
   private Single<LinkMetadata> unfurlLinkFromRemote(DankApi dankApi, Link link) {
---
+++ app/src/main/java/me/saket/dank/utils/MoshiStoreJsonParser.java
@@ -0,0 +1,29 @@
+package me.saket.dank.utils;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+
+import okio.BufferedSource;
+
+public class MoshiStoreJsonParser<KEY, VALUE> implements StoreFilePersister.JsonParser<KEY, VALUE> {
+  private final Moshi moshi;
+  private final Class<VALUE> valueType;
+
+  public MoshiStoreJsonParser(Moshi moshi, Class<VALUE> valueType) {
+    this.moshi = moshi;
+    this.valueType = valueType;
+  }
+
+  @Override
+  public VALUE fromJson(BufferedSource jsonBufferedSource) throws IOException {
+    JsonAdapter<VALUE> adapter = moshi.adapter(valueType);
+    return adapter.fromJson(jsonBufferedSource);
+  }
+
+  @Override
+  public String toJson(VALUE raw) {
+    return moshi.adapter(valueType).toJson(raw);
+  }
+}
