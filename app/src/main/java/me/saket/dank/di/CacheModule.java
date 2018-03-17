32310248c2bc833b3cddd6ddc4149b9c7d18855e
Saket Narayan <saket@saket.me>
2018-03-17 16:19:03 +0530

    Provide DiskLruCache through Dagger

--- app/src/main/java/me/saket/dank/di/CacheModule.java
+++ app/src/main/java/me/saket/dank/di/CacheModule.java
@@ -5,11 +5,13 @@ import android.app.Application;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
 import com.danikula.videocache.HttpProxyCacheServer;
+import com.jakewharton.disklrucache.DiskLruCache;
 import com.nytimes.android.external.cache3.Cache;
 import com.nytimes.android.external.cache3.CacheBuilder;
 import com.nytimes.android.external.fs3.filesystem.FileSystem;
 import com.nytimes.android.external.fs3.filesystem.FileSystemFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -19,8 +21,12 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import io.reactivex.Scheduler;
+import io.reactivex.exceptions.Exceptions;
 import io.reactivex.schedulers.Schedulers;
+import me.saket.dank.data.AppInfo;
+import me.saket.dank.data.FileSize;
 import me.saket.dank.data.links.Link;
+import me.saket.dank.utils.FileSizeUnit;
 
 @Module
 public class CacheModule {
@@ -73,5 +79,16 @@ public class CacheModule {
     return CacheBuilder.newBuilder()
         .maximumSize(100)
         .build();
+  }
+
+  @Provides
+  DiskLruCache diskLruCache(Application appContext, AppInfo appInfo) {
+    FileSize maxCacheSize = FileSize.create(100, FileSizeUnit.MB);
+    File cacheDirectory = new File(appContext.getCacheDir(), "disk_lru_cache");
+    try {
+      return DiskLruCache.open(cacheDirectory, appInfo.appVersionCode(), 1, (long) maxCacheSize.bytes());
+    } catch (IOException e) {
+      throw Exceptions.propagate(e);
+    }
   }
 }
