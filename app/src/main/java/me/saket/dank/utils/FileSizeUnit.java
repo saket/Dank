a264a994e5a97c817040316f8cd935591552ef13
Saket Narayan <saket@saket.me>
2018-03-17 15:06:06 +0530

    FileSizeUnit: rename size param in all conversion methods to 'size'

--- app/src/main/java/me/saket/dank/utils/FileSizeUnit.java
+++ app/src/main/java/me/saket/dank/utils/FileSizeUnit.java
@@ -11,90 +11,90 @@ import me.saket.dank.R;
 public enum FileSizeUnit {
 
   BYTES {
-    public double toBytes(double bytes) {
-      return bytes;
+    public double toBytes(double size) {
+      return size;
     }
 
-    public double toKiloBytes(double bytes) {
-      return bytes / 1024;
+    public double toKiloBytes(double size) {
+      return size / 1024;
     }
 
-    public double toMegaBytes(double bytes) {
-      return toKiloBytes(bytes) / 1024;
+    public double toMegaBytes(double size) {
+      return toKiloBytes(size) / 1024;
     }
 
-    public double toGigaBytes(double bytes) {
-      return toMegaBytes(bytes) / 1024;
+    public double toGigaBytes(double size) {
+      return toMegaBytes(size) / 1024;
     }
   },
 
   KB {
-    public double toBytes(double kiloBytes) {
-      return kiloBytes * 1024;
+    public double toBytes(double size) {
+      return size * 1024;
     }
 
-    public double toKiloBytes(double kiloBytes) {
-      return kiloBytes;
+    public double toKiloBytes(double size) {
+      return size;
     }
 
-    public double toMegaBytes(double kiloBytes) {
-      return kiloBytes / 1024;
+    public double toMegaBytes(double size) {
+      return size / 1024;
     }
 
-    public double toGigaBytes(double kiloBytes) {
-      return toMegaBytes(kiloBytes) / 1024;
+    public double toGigaBytes(double size) {
+      return toMegaBytes(size) / 1024;
     }
   },
 
   MB {
-    public double toBytes(double megaBytes) {
-      return toKiloBytes(megaBytes) * 1024;
+    public double toBytes(double size) {
+      return toKiloBytes(size) * 1024;
     }
 
-    public double toKiloBytes(double megaBytes) {
-      return megaBytes * 1024;
+    public double toKiloBytes(double size) {
+      return size * 1024;
     }
 
-    public double toMegaBytes(double megaBytes) {
-      return megaBytes;
+    public double toMegaBytes(double size) {
+      return size;
     }
 
-    public double toGigaBytes(double megaBytes) {
-      return megaBytes / 1024;
+    public double toGigaBytes(double size) {
+      return size / 1024;
     }
   },
 
   GB {
-    public double toBytes(double gigaBytes) {
-      return toKiloBytes(gigaBytes) * 1024;
+    public double toBytes(double size) {
+      return toKiloBytes(size) * 1024;
     }
 
-    public double toKiloBytes(double gigaBytes) {
-      return toMegaBytes(gigaBytes) * 1024;
+    public double toKiloBytes(double size) {
+      return toMegaBytes(size) * 1024;
     }
 
-    public double toMegaBytes(double gigaBytes) {
-      return gigaBytes * 1024;
+    public double toMegaBytes(double size) {
+      return size * 1024;
     }
 
-    public double toGigaBytes(double gigaBytes) {
-      return gigaBytes;
+    public double toGigaBytes(double size) {
+      return size;
     }
   };
 
-  public double toBytes(double megaBytes) {
+  public double toBytes(double size) {
     throw new AbstractMethodError();
   }
 
-  public double toKiloBytes(double megaBytes) {
+  public double toKiloBytes(double size) {
     throw new AbstractMethodError();
   }
 
-  public double toMegaBytes(double megaBytes) {
+  public double toMegaBytes(double size) {
     throw new AbstractMethodError();
   }
 
-  public double toGigaBytes(double megaBytes) {
+  public double toGigaBytes(double size) {
     throw new AbstractMethodError();
   }
 
