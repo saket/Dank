c7ed1d212ca515128d3334164b2b6bf1f7d304c7
Saket Narayan <saket@saket.me>
2018-03-28 09:30:25 +0530

    Downgrade support library to v27.0.2 to fix 'Program type already present' error

--- app/src/main/java/me/saket/dank/ui/DankDialogFragment.java
+++ app/src/main/java/me/saket/dank/ui/DankDialogFragment.java
@@ -1,6 +1,35 @@
 package me.saket.dank.ui;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
 import me.saket.dank.utils.lifecycle.LifecycleOwnerDialogFragment;
 
 public class DankDialogFragment extends LifecycleOwnerDialogFragment {
+
+  // TODO: Remove once we update support library to v27.1.0.
+  @NonNull
+  public Activity requireActivity() {
+    if (getActivity() == null) {
+      throw new AssertionError();
+    }
+    return getActivity();
+  }
+
+  @NonNull
+  public Object requireHost() {
+    if (getHost() == null) {
+      throw new AssertionError();
+    }
+    return getHost();
+  }
+
+  @NonNull
+  public Context requireContext() {
+    if (getContext() == null) {
+      throw new AssertionError();
+    }
+    return getContext();
+  }
 }
