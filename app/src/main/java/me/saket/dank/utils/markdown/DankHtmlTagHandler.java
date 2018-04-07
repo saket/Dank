84529bc0879a80b93ad3cc0625869ae6f65cf8e1
Saket Narayan <saket@saket.me>
2018-04-07 11:28:52 +0530

    Markdown: normalize lists inside blockquotes

--- app/src/main/java/me/saket/dank/utils/markdown/DankHtmlTagHandler.java
+++ app/src/main/java/me/saket/dank/utils/markdown/DankHtmlTagHandler.java
@@ -2,14 +2,22 @@ package me.saket.dank.utils.markdown;
 
 import android.graphics.Color;
 import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
 import android.text.Editable;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.xml.sax.XMLReader;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 
 import me.saket.dank.markdownhints.MarkdownHintOptions;
 import me.saket.dank.widgets.span.HorizontalRuleSpan;
+import timber.log.Timber;
 
 public class DankHtmlTagHandler extends HtmlTagHandler {
 
@@ -30,14 +38,36 @@ public class DankHtmlTagHandler extends HtmlTagHandler {
    */
   String overrideTags(@Nullable String html) {
     //noinspection ConstantConditions
-    return html
+    String htmlWithoutParagraphsInList = html
+        // Reddit sends paragraphs inside <li> items, which doesn't make sense?
+        .replace("<p>", "<div>")
+        .replace("</p>", "</div>")
+        .replace("<li>\\s*<div>", "<li>")
+        .replace("</div>\\s*</li>", "</li>")
+        .replace("<li><div>", "<li>")
+        .replace("</div></li>", "</li>");
+
+    if (html.contains("<blockquote>")) {
+      // Not sure if it's Android or Dank's HTML parser, but lists
+      // inside block-quotes aren't rendered very nicely.
+      htmlWithoutParagraphsInList = normalizeListsInsideBlockQuotes(htmlWithoutParagraphsInList);
+    }
+
+    return htmlWithoutParagraphsInList
+        // Wrap within a division to add spacing around the HR.
+        .replace("<hr/>", "<div><hr/></div>")
+
+        // Progressive reduction of text
+        .replace("<sup>", "<sup><small>")
+        .replace("</sup>", "</small></sup>")
+
+        // WARNING: THESE SHOULD BE AT THE LAST.
         .replace("<ul", "<" + HtmlTagHandler.UNORDERED_LIST)
         .replace("</ul>", "</" + HtmlTagHandler.UNORDERED_LIST + ">")
         .replace("<ol", "<" + HtmlTagHandler.ORDERED_LIST)
         .replace("</ol>", "</" + HtmlTagHandler.ORDERED_LIST + ">")
         .replace("<li", "<" + HtmlTagHandler.LIST_ITEM)
         .replace("</li>", "</" + HtmlTagHandler.LIST_ITEM + ">")
-        .replace("<hr/>", "<div><hr/></div>")   // Wrap within a division to add spacing around the HR.
         ;
   }
 
@@ -54,5 +84,116 @@ public class DankHtmlTagHandler extends HtmlTagHandler {
     int start = output.length();
     output.append(" \n");   // Paragraph styles like LineBackgroundSpan need to end with a new line.
     output.setSpan(new HorizontalRuleSpan(Color.DKGRAY, 4f), start, output.length(), 0);
+  }
+
+  @VisibleForTesting
+  static String normalizeListsInsideBlockQuotes(String html) {
+    try {
+      Document document = Jsoup.parse(html);
+      document.outputSettings(new Document.OutputSettings().prettyPrint(false));
+
+      Elements blockQuotes = document.select("blockquote");
+      for (Element blockQuote : blockQuotes) {
+        String quoteContent = blockQuote.toString();
+        if (quoteContent.contains("<ol") || quoteContent.contains("<ul")) {
+          String quoteWithEscapedSingleQuotes = escapeSingleQuotes(quoteContent);
+          int indexOfContentToBeModified = html.indexOf(quoteWithEscapedSingleQuotes);
+          int lengthOfContentToBeModified = quoteWithEscapedSingleQuotes.length();
+
+          String quoteContentWithoutLists = normalizeList(quoteContent);
+          quoteContentWithoutLists = escapeSingleQuotes(quoteContentWithoutLists);
+
+          html = html.substring(0, indexOfContentToBeModified)
+              + quoteContentWithoutLists
+              + html.substring(indexOfContentToBeModified + lengthOfContentToBeModified);
+        }
+      }
+    } catch (Exception e) {
+      Timber.e(e, "Couldn't normalize list inside blockquotes: " + html);
+    }
+    return html;
+  }
+
+  static String escapeSingleQuotes(String s) {
+    return s.replaceAll("'", "&#39;");
+  }
+
+  /**
+   * Copied from Slide app. I have no trust on this code.
+   */
+  private static String normalizeList(String html) {
+    int firstIndex;
+    boolean isNumbered;
+    int firstOl = html.indexOf("<ol");
+    int firstUl = html.indexOf("<ul");
+
+    if ((firstUl != -1 && firstOl > firstUl) || firstOl == -1) {
+      firstIndex = firstUl;
+      isNumbered = false;
+    } else {
+      firstIndex = firstOl;
+      isNumbered = true;
+    }
+    List<Integer> listNumbers = new ArrayList<>();
+    int indent = -1;
+
+    int i = firstIndex;
+    while (i < html.length() - 4 && i != -1) {
+      if (html.substring(i, i + 3).equals("<ol") || html.substring(i, i + 3).equals("<ul")) {
+        if (html.substring(i, i + 3).equals("<ol")) {
+          isNumbered = true;
+          indent++;
+          listNumbers.add(indent, 1);
+        } else {
+          isNumbered = false;
+        }
+        i = html.indexOf("<li", i);
+      } else if (html.substring(i, i + 3).equals("<li")) {
+        int tagEnd = html.indexOf(">", i);
+        int itemClose = html.indexOf("</li", tagEnd);
+        int ulClose = html.indexOf("<ul", tagEnd);
+        int olClose = html.indexOf("<ol", tagEnd);
+        int closeTag;
+
+        // Find what is closest: </li>, <ul>, or <ol>
+        if (((ulClose == -1 && itemClose != -1) || (itemClose != -1 && ulClose != -1 && itemClose < ulClose)) && ((olClose == -1 && itemClose != -1) || (itemClose != -1 && olClose != -1 && itemClose < olClose))) {
+          closeTag = itemClose;
+        } else if (((ulClose == -1 && olClose != -1) || (olClose != -1 && ulClose != -1 && olClose < ulClose)) && ((olClose == -1 && itemClose != -1) || (olClose != -1 && itemClose != -1 && olClose < itemClose))) {
+          closeTag = olClose;
+        } else {
+          closeTag = ulClose;
+        }
+
+        String text = html.substring(tagEnd + 1, closeTag);
+        String indentSpacing = "";
+        for (int j = 0; j < indent; j++) {
+          indentSpacing += "&nbsp;&nbsp;&nbsp;&nbsp;";
+        }
+        if (isNumbered) {
+          html = html.substring(0, tagEnd + 1)
+              + indentSpacing +
+              listNumbers.get(indent) + ". " +
+              text + "<br/>" +
+              html.substring(closeTag);
+          listNumbers.set(indent, listNumbers.get(indent) + 1);
+          i = closeTag + 3;
+        } else {
+          html = html.substring(0, tagEnd + 1) + indentSpacing + "• " + text + "<br/>" + html.substring(closeTag);
+          i = closeTag + 2;
+        }
+      } else {
+        i = html.indexOf("<", i + 1);
+        if (i != -1 && html.substring(i, i + 4).equals("</ol")) {
+          indent--;
+          if (indent == -1) {
+            isNumbered = false;
+          }
+        }
+      }
+    }
+
+    html = html.replace("<ol>", "").replace("<ul>", "").replace("<li>", "").replace("</li>", "").replace("</ol>", "").replace("</ul>", ""); //Remove the tags, which actually work in Android 7.0 on
+
+    return html;
   }
 }
