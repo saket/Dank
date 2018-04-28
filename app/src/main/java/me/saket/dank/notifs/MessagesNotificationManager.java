8ef804dc01954ddbc1b078acaf8925993e761564
Saket Narayan <saket@saket.me>
2018-04-29 01:27:02 +0530

    Rename @drawable/ic_status_bar -> @drawable/ic_status_bar_24dp

--- app/src/main/java/me/saket/dank/notifs/MessagesNotificationManager.java
+++ app/src/main/java/me/saket/dank/notifs/MessagesNotificationManager.java
@@ -296,7 +296,7 @@ public class MessagesNotificationManager {
             .setStyle(new NotificationCompat.BigTextStyle().bigText(markdownStrippedBody))
             .setShowWhen(true)
             .setWhen(JrawUtils.createdTimeUtc(unreadMessage))
-            .setSmallIcon(R.drawable.ic_status_bar)
+            .setSmallIcon(R.drawable.ic_status_bar_24dp)
             .setGroup(NotificationConstants.UNREAD_MESSAGE_BUNDLE_NOTIFS_GROUP_KEY)
             .setAutoCancel(true)
             .setColor(ContextCompat.getColor(context, R.color.color_accent))
@@ -331,7 +331,7 @@ public class MessagesNotificationManager {
         .setStyle(new NotificationCompat.BigTextStyle()
             .bigText(markdownStrippedBody)
             .setSummaryText(loggedInUserName))
-        .setSmallIcon(R.drawable.ic_status_bar)
+        .setSmallIcon(R.drawable.ic_status_bar_24dp)
         .setDeleteIntent(deletePendingIntent)
         .addAction(markAsReadAction);
   }
@@ -404,7 +404,7 @@ public class MessagesNotificationManager {
     return new NotificationCompat.Builder(context, context.getString(R.string.notification_channel_unread_messages_id))
         .setContentTitle(notificationTitle)
         .setContentText(notifBody)
-        .setSmallIcon(R.drawable.ic_status_bar)
+        .setSmallIcon(R.drawable.ic_status_bar_24dp)
         .setStyle(messagingStyleBuilder)
         .setDeleteIntent(summaryDeletePendingIntent)
         .addAction(markAllReadAction);
