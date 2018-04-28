23e098fc652c01617a7906a78ef95f9d6a730c18
Saket Narayan <saket@saket.me>
2018-04-29 01:14:44 +0530

    fixup! Show welcome text on first start

--- app/src/main/java/me/saket/dank/walkthrough/SubmissionGesturesWalkthrough.java
+++ app/src/main/java/me/saket/dank/walkthrough/SubmissionGesturesWalkthrough.java
@@ -1,18 +1,22 @@
 package me.saket.dank.walkthrough;
 
+import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.f2prateek.rx.preferences2.Preference;
 import com.google.auto.value.AutoValue;
-import io.reactivex.Observable;
+
 import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import io.reactivex.Observable;
 import me.saket.dank.R;
 import me.saket.dank.ui.subreddit.SubmissionSwipeActionsProvider;
 import me.saket.dank.ui.subreddit.SubredditSubmissionsAdapter;
@@ -21,6 +25,7 @@ import me.saket.dank.utils.Optional;
 import me.saket.dank.widgets.swipe.SwipeAction;
 import me.saket.dank.widgets.swipe.SwipeActionIconView;
 import me.saket.dank.widgets.swipe.SwipeActions;
+import me.saket.dank.widgets.swipe.SwipeTriggerRippleDrawable;
 import me.saket.dank.widgets.swipe.SwipeableLayout;
 import me.saket.dank.widgets.swipe.SwipeableLayout.SwipeActionIconProvider;
 import me.saket.dank.widgets.swipe.ViewHolderWithSwipeActions;
@@ -60,25 +65,20 @@ public class SubmissionGesturesWalkthrough {
   }
 
   public static class ViewHolder extends RecyclerView.ViewHolder implements ViewHolderWithSwipeActions {
-
-    private final ImageView thumbnailView;
     private final TextView titleView;
-    private final TextView bylineView;
+    private final TextView messageView;
 
     protected ViewHolder(View itemView) {
       super(itemView);
-      thumbnailView = itemView.findViewById(R.id.submission_item_icon);
-      titleView = itemView.findViewById(R.id.submission_item_title);
-      bylineView = itemView.findViewById(R.id.submission_item_byline);
+      titleView = itemView.findViewById(R.id.submissiongestureswalkthrough_item_title);
+      messageView = itemView.findViewById(R.id.submissiongestureswalkthrough_item_message);
 
-      titleView.setText("Hey there! Welcome to Dank.");
-      thumbnailView.setImageResource(R.drawable.ic_adb_24dp);
-      thumbnailView.setBackgroundResource(R.color.blue_gray_800);
-      bylineView.setText("Swipe here horizontally to reveal gestures");
+      titleView.setText(R.string.subreddit_gestureswalkthrough_title);
+      messageView.setText(R.string.subreddit_gestureswalkthrough_message);
     }
 
     public static ViewHolder create(LayoutInflater inflater, ViewGroup parent) {
-      View itemView = inflater.inflate(R.layout.list_item_submission, parent, false);
+      View itemView = inflater.inflate(R.layout.list_item_submission_gestures_walkthrough, parent, false);
       return new ViewHolder(itemView);
     }
 
@@ -104,7 +104,7 @@ public class SubmissionGesturesWalkthrough {
       swipeableLayout.setSwipeActions(swipeActionsProvider.actions());
       swipeableLayout.setSwipeActionIconProvider(swipeActionsProvider);
       swipeableLayout.setOnPerformSwipeActionListener(action ->
-          swipeActionsProvider.perform(action, holder)
+          swipeActionsProvider.perform(action, holder, swipeableLayout)
       );
       return holder;
     }
@@ -120,12 +120,17 @@ public class SubmissionGesturesWalkthrough {
   }
 
   public static class WalkthroughSwipeActionsProvider implements SwipeActionIconProvider {
-
     private final SubmissionSwipeActionsProvider submissionSwipeActionsProvider;
+    private final Preference<Boolean> hasUserLearnedPref;
+    private int discoveryCount = 0;
 
     @Inject
-    public WalkthroughSwipeActionsProvider(SubmissionSwipeActionsProvider submissionSwipeActionsProvider) {
+    public WalkthroughSwipeActionsProvider(
+        SubmissionSwipeActionsProvider submissionSwipeActionsProvider,
+        @Named("user_learned_submission_gestures") Preference<Boolean> hasUserLearnedPref)
+    {
       this.submissionSwipeActionsProvider = submissionSwipeActionsProvider;
+      this.hasUserLearnedPref = hasUserLearnedPref;
     }
 
     public SwipeActions actions() {
@@ -137,8 +142,26 @@ public class SubmissionGesturesWalkthrough {
       submissionSwipeActionsProvider.showSwipeActionIcon(imageView, oldAction, newAction);
     }
 
-    public void perform(SwipeAction action, ViewHolder holder) {
+    public void perform(SwipeAction action, ViewHolder holder, SwipeableLayout swipeableLayout) {
+      swipeableLayout.playRippleAnimation(action, SwipeTriggerRippleDrawable.RippleType.REGISTER);
 
+      ++discoveryCount;
+
+      Context context = swipeableLayout.getContext();
+      holder.titleView.setText(String.format("'%s'", context.getString(action.labelRes())));
+      holder.titleView.setTextColor(ContextCompat.getColor(context, action.backgroundColorRes()));
+
+      switch (discoveryCount) {
+        case 1:
+          holder.messageView.setText(R.string.subreddit_gestureswalkthrough_message_after_first_swipe_action);
+          break;
+
+        default:
+        case 2:
+          holder.messageView.setText(R.string.subreddit_gestureswalkthrough_message_after_second_swipe_action);
+          holder.itemView.setOnClickListener(o -> hasUserLearnedPref.set(true));
+          break;
+      }
     }
   }
 }
