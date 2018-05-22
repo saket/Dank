0d5980858a3f825140fc443c603fbad9a1870dd8
Saket Narayan <saket@saket.me>
2018-05-22 23:26:48 +0530

    Rename ChangeCommentSortingClicked -> SubmissionChangeCommentSortClicked

--- app/src/main/java/me/saket/dank/ui/submission/SubmissionUi.java
+++ app/src/main/java/me/saket/dank/ui/submission/SubmissionUi.java
@@ -2,7 +2,7 @@ package me.saket.dank.ui.submission;
 
 import net.dean.jraw.models.Submission;
 
-import me.saket.dank.ui.submission.events.ChangeCommentSortingClicked;
+import me.saket.dank.ui.submission.events.SubmissionChangeCommentSortClicked;
 import me.saket.dank.utils.DankSubmissionRequest;
 
 interface SubmissionUi {
@@ -13,7 +13,7 @@ interface SubmissionUi {
 
   void acceptRequest(DankSubmissionRequest lastRequest);
 
-  void showChangeSortPopup(ChangeCommentSortingClicked event, DankSubmissionRequest activeRequest);
+  void showChangeSortPopup(SubmissionChangeCommentSortClicked event, DankSubmissionRequest activeRequest);
 
   void acceptSubmission(Submission submission);
 }
