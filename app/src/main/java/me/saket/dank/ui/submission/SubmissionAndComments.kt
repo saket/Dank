504e6cd4559ffbc35e25dac197e8d92c22453f39
Saket Narayan <saket@saket.me>
2018-06-03 17:13:07 +0530

    [JRAW v1.0] Add comments to synthetic-submission-for-gestures-walkthrough

--- app/src/main/java/me/saket/dank/ui/submission/SubmissionAndComments.kt
+++ app/src/main/java/me/saket/dank/ui/submission/SubmissionAndComments.kt
@@ -1,10 +1,12 @@
 package me.saket.dank.ui.submission
 
+import com.squareup.moshi.JsonClass
 import me.saket.dank.utils.Optional
 import net.dean.jraw.models.Submission
 import net.dean.jraw.tree.RootCommentNode
 
 // TODO: Merge this with CachedSubmissionAndComments.
+@JsonClass(generateAdapter = true)
 data class SubmissionAndComments constructor(val submission: Submission, val comments: Optional<RootCommentNode> = Optional.empty()) {
 
   companion object {
